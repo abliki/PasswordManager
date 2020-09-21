73dd62038644a5519fc1f30ffade19dfea78570d
Oliver Zhang <yzhang58@email.wm.edu>
2020-09-21 15:16:25 -0400

    random password generation implemented

--- app/src/main/java/com/example/passwordmanager/PasswordRandomGeneration.java
+++ app/src/main/java/com/example/passwordmanager/PasswordRandomGeneration.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -14,19 +16,43 @@ import java.io.StringWriter;
 
 public class PasswordRandomGeneration extends AppCompatActivity {
     private boolean includeLower = true, includeNumber = true, includeUpper = false, includeSpecial = false;
+    TextView passwordGenText, passwordLengthHint;
+    Button buttonGenerate, buttonConfirm;
+    Switch lowerSwitch, upperSwitch, numberSwitch, specialSwitch;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_random_generation);
 
-        TextView passwordGenText = findViewById(R.id.password_random_text_hint);
-        Button buttonGenerate = findViewById(R.id.generate_new_pw_button);
-        Button buttonConfirm = findViewById(R.id.confirm_pw_button);
-        Switch lowerSwitch = findViewById(R.id.lower_case_label);
-        Switch upperSwitch = findViewById(R.id.upper_case_label);
-        Switch numberSwitch = findViewById(R.id.number_label);
-        Switch specialSwitch = findViewById(R.id.special_character_label);
+        passwordGenText = findViewById(R.id.password_random);
+        passwordLengthHint = findViewById(R.id.password_length_hint);
+        buttonGenerate = findViewById(R.id.generate_new_pw_button);
+        buttonConfirm = findViewById(R.id.confirm_pw_button);
+        lowerSwitch = findViewById(R.id.lower_case_label);
+        upperSwitch = findViewById(R.id.upper_case_label);
+        numberSwitch = findViewById(R.id.number_label);
+        specialSwitch = findViewById(R.id.special_character_label);
+        seekBar = findViewById(R.id.password_gen_length_seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                passwordLengthHint.setText("Password Length: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        generatePw();
     }
 
     /**
@@ -38,7 +64,7 @@ public class PasswordRandomGeneration extends AppCompatActivity {
      * @param passwordLength String: a string representation of the number of digits in the password
      * @return a randomized password based on the given requirements
      */
-    public String passwordGenerator(boolean lower, boolean upper, boolean numbers, boolean symbols, String passwordLength) {
+    public String passwordGen(boolean lower, boolean upper, boolean numbers, boolean symbols, int passwordLength) {
         StringBuilder password = new StringBuilder();
         String chars = "";
         if (lower)
@@ -49,14 +75,33 @@ public class PasswordRandomGeneration extends AppCompatActivity {
             chars += "0123456789";
         if (symbols)
             chars += "#@?;:/*-+_#$%&!";
-        int length = Integer.parseInt(passwordLength);
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < passwordLength; i++) {
             int rand = (int) (Math.random() * chars.length());
             password.append(chars.charAt(rand));
         }
         return password.toString();
     }
 
+    private void generatePw() {
+        // change the parameter status based on user input
+        includeLower = lowerSwitch.isChecked();
+        includeUpper = upperSwitch.isChecked();
+        includeNumber = numberSwitch.isChecked();
+        includeSpecial = specialSwitch.isChecked();
+        int progress = seekBar.getProgress();
+
+        String pw = passwordGen(includeLower, includeUpper, includeNumber, includeSpecial, progress);
+        passwordGenText.setText(pw);
+
+        // set the text size if the string is too long or too short
+        if (pw.length() > 12)
+            passwordGenText.setTextSize(36);
+        else
+            passwordGenText.setTextSize(48);
+    }
+
+    // onClick handler of the "generate" button
     public void generateRandomPassword(View view) {
+        generatePw();
     }
 }
